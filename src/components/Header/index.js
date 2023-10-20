40a86fa1afcdd417bbd5c891af974b309609c96d
shiva <palleshivaram24@gmail.com>
2023-10-20 16:50:16 +0000

    changes

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import './index.css'
+import {Link} from 'react-router-dom'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="blog-container">
+      <h1 className="blog-title">My Blog</h1>
+      <ul className="nav-menu">
+        <ul className="nav-menu">
+          <li>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Header
